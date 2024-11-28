fb6ce46faafb445cb3bb9ac9f3a8d563044dd891
IanKoech <iankoech9@gmail.com>
2024-11-28 21:17:30 +0300

    :construction: Bootstrapped application

--- obj/Debug/net8.0/game-store-api.AssemblyInfo.cs
+++ obj/Debug/net8.0/game-store-api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("game-store-api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa441a6a08c780d8711cc6df9c849ff4b7d651dd")]
 [assembly: System.Reflection.AssemblyProductAttribute("game-store-api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("game-store-api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
